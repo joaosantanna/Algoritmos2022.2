4854ca5f2eea7c85e532794d3ced8b62cea9677b
Joao Santanna <joaosantanna@yahoo.com.br>
2023-04-03 11:48:04 -0300

    Atualizacao questoes resolvidas em 3 de Abril 2023

--- Lista 1/Q3.py
+++
@@ -1,9 +0,0 @@
-frase = input('Digite uma frase:')
-
-vogais = 'aeiouAEIOU'
-vogais = list(vogais)
-quantidade_vogais = 0
-for v in vogais:
-    quantidade_vogais += frase.count(v)
-
-print(f'Quantidade de vogais = {quantidade_vogais}')
